6d3b31577b17d546aafb58e916885c4e86c1cc82
Thu Phan <alicebe.phan@gmail.com>
2021-10-14 23:25:41 -0500

    Imported Manager, Engineer, Intern from their files to index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHTML = require("./utils/generateHTML.js");
+const generateHTML = require('./utils/generateHTML.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
 const teamMemberArray = [];
 
 // created inquirer prompts for manager
