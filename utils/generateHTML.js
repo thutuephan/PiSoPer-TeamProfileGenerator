3d32809ebb7acc987c5da8635e9b44d812b298ca
Thu Phan <alicebe.phan@gmail.com>
2021-10-16 16:45:06 -0500

    testing html styling

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -30,101 +30,113 @@ function generateHTML(empArray) {
 // create generate manager card
 function managerCard(manager) {
     return `
-    <div class="mt-4">
-        <div class="card h-100">
-            <div class="card-header">
-                <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="fas fa-mug-hot"></i>
-
+    <div class="col-lg-3 col-md-4 col-sm-3">
+            <div class="shadow d-flex 
+            justify-content-center
+            align-items-center p-3 bg-light rounded-lg
+            flex-column">
+            <div class="icon">
+                <i class="fas fa-mug-hot"></i>
+        
             </div>
-        <div class="card-body">
-            <p class="id">ID: ${manager.id}</p>
-            <p class="email">Email: ${manager.email}</p>
-            <p class="officenumber">Office Number: ${manager.officeNumber}</p>
-        </div>
+            <div class="card-header my-2">
+                <h3 class="text-grey">${manager.name}</h3>
+                <h4 class="text-grey">Manager</h4>
+            </div>
+            <div class="card-body">
+                <p class="text-grey id">ID: ${manager.id}</p>
+                <p class="text-grey email">Email: ${manager.email}</p>
+                <p class="text-grey officenumber">Office Number: ${manager.officeNumber}</p>
+            </div>
+            
+            </div>
     </div>
     `;
-
-
 }
 
 // create engineer card
 function engineerCard(engineer) {
     return `
-    <div class="mt-4">
-        <div class="card h-100">
-            <div class="card-header">
-                <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="fas fa-glasses"></i>
+    <div class="col-lg-3 col-md-4 col-sm-3">
+            <div class="shadow d-flex 
+            justify-content-center
+            align-items-center p-3 bg-light rounded-lg
+            flex-column">
+            <div class="icon">
+                <i class="fas fa-glasses"></i>
+                        
             </div>
-        </div>
+            <div class="card-header my-2">
+                <h3 class="text-grey">${engineer.name}</h3>
+                <h4 class="text-grey">Engineer</h4>
+            </div>
+    
+            <div class="card-body">
+                <p class="text-grey id">ID: ${engineer.id}</p>
+                <p class="text-grey email">Email: ${engineer.email}</p>
+                <p class="text-grey github">GitHub: ${engineer.github}</p>
+            </div>
+            </div>
 
-        <div class="card-body">
-            <p class="id">ID: ${engineer.id}</p>
-            <p class="email">Email: ${engineer.email}</p>
-            <p class="github">GitHub: ${engineer.github}</p>
-        </div>
     </div>
+            
     `;
 }
 
 // create intern card
 function internCard(intern) {
     return `
-    <div class="mt-4">
-        <div class="card h-100">
-            <div class="card-header">
-                <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="fas fa-user-graduate"></i>
-
-
+    <div class="col-lg-3 col-md-4 col-sm-3">
+            <div class="shadow d-flex 
+            justify-content-center
+            align-items-center p-3 bg-light rounded-lg
+            flex-column">
+            <div class="icon">
+                <i class="fas fa-user-graduate"></i>
+                        
+            </div>
+            <div class="card-header my-2">
+                <h3 class="text-grey">${intern.name}</h3>
+                <h4 class="text-grey">Engineer</h4>
+            </div>
+    
+            <div class="card-body">
+                <p class="text-grey id">ID: ${intern.id}</p>
+                <p class="text-grey email">Email: ${intern.email}</p>
+                <p class="text-grey github">School: ${intern.school}</p>
+            </div>
             </div>
 
-        <div class="card-body">
-            <p class="id">ID: ${intern.id}</p>
-            <p class="email">Email: ${intern.email}</p>
-            <p class="school">School: ${intern.school}</p>
-        </div>
     </div>
-
+    
     `;
 }
 
 
 function finalHTML() {
     return `
-    
-    <!DOCTYPE html>
+    <!doctype html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Amazing Team Profile</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="./src/style.css">
+        <title>Amazing Team Profile</title>
     </head>
     <body>
-        <header>
-            <nav class="nav" id="navbar">
-                <span class="nav justify-content-center" id="nav-text">My Team</span>
-            </nav>
-        
-        </header>
-        <main>
-            <div class="container">
-                <div class="row justify-content" id="team-card">
-
-                ${cardArray.join("")}
+        <section class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-white text-center my-5">Meet My Team</h2>
+                        ${cardArray.join("")}
                 </div>
             </div>
-        </main>
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  
+        </section>
+
+    
     </body>
     </html>
    `;
